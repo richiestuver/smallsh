b5b692ae5daf9c3ec726c1aa7c611d8f48b76eb4
richiestuver <stuverr@oregonstate.edu>
2021-10-28 15:21:30 -0700

    implement built in cd

--- smallsh.c
+++ smallsh.c
@@ -23,6 +23,43 @@ Created: 10-20-21
 #include "parser.h"
 #include "smallsh.h"
 
+char* BUILTIN_COMMANDS[] = { "exit", "cd", "status" };
+
+/* function exit
+exits the shell and terminates all other processes or jobs that the shell has started.
+returns: 0
+*/
+
+void builtin_exit(void)
+{
+    if (DEBUG) {
+        printf("(builtin_exit %d) terminating shell...\n", getpid());
+    }
+    exit(0);
+}
+
+void builtin_cd(struct command* command)
+{
+
+    char* dest = *(command->argv + 1);
+
+    if (dest == NULL) {
+        char* HOME = getenv("HOME");
+        printf("(builtin_cd %d) changing to HOME dir %s\n", getpid(), HOME);
+
+        dest = HOME;
+    }
+
+    if (chdir(dest) != 0) {
+        printf("(builtin_cd %d) cannot change to dir %s ", getpid(), dest);
+        fflush(stdout);
+        perror("(chdir)");
+    };
+
+    char* cwd = NULL;
+    printf("(builtin_cd %d) %s\n", getpid(), getcwd(cwd, 0));
+}
+
 int main(void)
 {
     char* user_input;
@@ -35,7 +72,8 @@ int main(void)
 
         if (user_input == NULL) {
             if (DEBUG) {
-                printf("(main) user input is NULL...\n");
+                printf("(main %d) user input is NULL...\n", getpid());
+                fflush(stdout);
             }
             continue;
         };
@@ -43,7 +81,7 @@ int main(void)
         command = parse(user_input);
 
         if (DEBUG) {
-            printf("(main) fully parsed command struct is: \n");
+            printf("(main %d) fully parsed command struct is: \n", getpid());
             printf("        command (arg0): %s\n", *(command->argv + 0));
             int i = 1;
             while (*(command->argv + i) != NULL) {
@@ -57,7 +95,19 @@ int main(void)
             printf("        stderr: %s\n", (command->f_stderr));
         }
 
-        launch(command);
+        // TODO: builtin commands for exit, cd and status
+
+        if (strcmp(*(command->argv), "exit") == 0) {
+            printf("(main %d) LAUNCHING exit\n", getpid());
+            fflush(stdout);
+            builtin_exit();
+        } else if (strcmp(*(command->argv), "cd") == 0) {
+            printf("(main %d) LAUNCHING cd\n", getpid());
+            fflush(stdout);
+            builtin_cd(command);
+        } else {
+            launch(command);
+        }
     }
     return 0;
 }
