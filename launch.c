fcd032b8af40bb33782da8f4c21263fae652d72f
richiestuver <stuverr@oregonstate.edu>
2021-10-30 15:41:58 -0700

    revert to breaking out of flow if waitpid errors out

--- launch.c
+++ launch.c
@@ -127,7 +127,7 @@ void launch(struct command* command, struct status* status)
                     perror("(waitpid)");
                     fflush(stdout);
                 }
-                child_pid = spawn_pid;
+                break;
             }
 
             if WIFEXITED (child_status) {
