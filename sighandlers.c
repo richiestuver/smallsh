df4bab1d1c71262495b21aeb81c72b00d51539b3
richiestuver <stuverr@oregonstate.edu>
2021-10-30 15:59:04 -0700

    fix warnings related to missing braces in struct initialization

--- sighandlers.c
+++ sighandlers.c
@@ -18,7 +18,7 @@ for specific signals.
 */
 void register_signal(int signal, void (*handler)(int))
 {
-    struct sigaction catch = { 0 };
+    struct sigaction catch = { { 0 } };
     catch.sa_handler = handler;
     catch.sa_flags = 0;
 
@@ -61,7 +61,7 @@ void register_SIGTSTP(void (*handler)(int))
 
 sigset_t* block(int signal)
 {
-    sigset_t old_set = { 0 };
+    sigset_t old_set = { { 0 } };
     sigset_t* block_set = NULL;
     block_set = malloc(sizeof(sigset_t));
     sigemptyset(block_set);
@@ -73,7 +73,7 @@ sigset_t* block(int signal)
 
 sigset_t* unblock(sigset_t* block_set)
 {
-    sigset_t old_set = { 0 };
+    sigset_t old_set = { { 0 } };
     sigprocmask(SIG_UNBLOCK, block_set, &old_set);
     return block_set;
 }
