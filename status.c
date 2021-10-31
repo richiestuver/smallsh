c2775eb720c84494b29ce85b5f6f09cf85200dab
richiestuver <stuverr@oregonstate.edu>
2021-10-30 17:27:11 -0700

    documentation

--- status.c
+++ status.c
@@ -6,6 +6,13 @@
 
 #include "status.h"
 
+/* function init_status
+initializes storage and sets default values for a new
+status struct to be used to store latest information about
+last exited foreground process.
+
+returns: pointer to initialized default status struct
+*/
 struct status* init_status()
 {
 
@@ -21,6 +28,15 @@ struct status* init_status()
     return status;
 }
 
+/* function update_status
+updates the provided status struct with new data.
+
+status: pointer to status struct to update
+pid: pid_t representing last foreground process
+name: command name for last foreground process
+code: status signal or exit code of last foreground process
+exit_type: EXIT or SIGNAL
+*/
 struct status* update_status(struct status* status, pid_t pid, char* name, int code, enum EXIT_TYPE exit_type)
 {
 
--- status.h
+++ status.h
@@ -16,8 +16,25 @@ struct status {
     bool signaled;
     int code;
 };
+
+/* function init_status
+initializes storage and sets default values for a new
+status struct to be used to store latest information about
+last exited foreground process.
+
+returns: pointer to initialized default status struct
+*/
 struct status* init_status();
 
+/* function update_status
+updates the provided status struct with new data.
+
+status: pointer to status struct to update
+pid: pid_t representing last foreground process
+name: command name for last foreground process
+code: status signal or exit code of last foreground process
+exit_type: EXIT or SIGNAL
+*/
 struct status* update_status(struct status* status, pid_t pid, char* name, int code, enum EXIT_TYPE exit_type);
 
 #endif
