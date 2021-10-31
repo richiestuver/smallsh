9d8e864500bb890320b9b4c91a7869543c778bb7
richiestuver <stuverr@oregonstate.edu>
2021-10-30 17:11:35 -0700

    documentation

--- exec_env.c
+++ exec_env.c
@@ -3,6 +3,12 @@
 
 #include "exec_env.h"
 
+/* function init_exec_env
+allocates storage for a new execution environment
+and returns a pointer to this struct. By default
+background processes are enabled. Other global settings
+pertaining to command execution may be stored here.
+*/
 struct exec_env* init_exec_env(void)
 {
     struct exec_env* exec_env = NULL;
@@ -11,4 +17,9 @@ struct exec_env* init_exec_env(void)
     return exec_env;
 }
 
+/* exec_env declaration provides global
+availability across smallsh program to share and
+update the same execution environment.
+declare with extern to access in other files.
+*/
 struct exec_env* exec_env = NULL;
--- exec_env.h
+++ exec_env.h
@@ -1,10 +1,21 @@
 #ifndef EXEC_ENV_H
 #define EXEC_ENV_H
 
+/* exec_env declaration provides global
+availability across smallsh program to share and
+update the same execution environment.
+declare with extern to access in other files.
+*/
 struct exec_env {
     bool background_enabled;
 };
 
+/* function init_exec_env
+allocates storage for a new execution environment
+and returns a pointer to this struct. By default
+background processes are enabled. Other global settings
+pertaining to command execution may be stored here.
+*/
 struct exec_env* init_exec_env(void);
 
 #endif
